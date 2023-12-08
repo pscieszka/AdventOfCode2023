21bdc24dbb61ebf3aed8e06df92c7846eae1f546
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-12-08 14:39:04 +0100

    day8

--- adventOfCode/adventOfCode/Day8.h
+++ adventOfCode/adventOfCode/Day8.h
@@ -1,60 +1,67 @@
-#pragma once
-
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
-#include <sstream>
-using namespace std;
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
 
 void calculateDay8() {
+    std::ifstream file("input.txt");
+    std::string line;
 
-    vector<vector<string>> vec;
-    ifstream file("input.txt");
-    string line;
-    string in = "LRRLRRLRLLLRLLRLRRLRRLRRLRRLLRLLRRRLRRRLRRLLLRLRRLLLLLRRRLRRRLRRRLRRLRRLRLRLRLRLRRRLRRRLRRRLRRLRRLRLRLRRLLRRRLLRRLRRLRRRLRLLRRLRRLRRRLRRRLRRRLRRRLRRLLLRRRLLRRLLLRRLRRLLRRLRRRLRRLRRLRRRLRRLLLRLRRRLLRRRLRLRRLRLRLRLRRRLRLRLRRLLRRLRRLRRLRRLLRLRLRRRLRRLRRLRRLRLRRRLRRLRLLRRLLRRLRLLLRLLRRRLRLRLLRRRR";
-    string str;
-    string str2;
-    string str3;
+    std::vector<std::vector<std::string>> ws;
     while (std::getline(file, line)) {
-        str = line.substr(0,3);
-        str2 = line.substr(7, 3);
-        str3 = line.substr(12, 3);
-        vector<string> temp = { str,str2,str3 };
-        vec.push_back(temp);
+        std::vector<std::string> tokens;
+        size_t pos = 0;
+        while ((pos = line.find(" ")) != std::string::npos) {
+            tokens.push_back(line.substr(0, pos));
+            line.erase(0, pos + 1);
+        }
+        tokens.push_back(line);
+        ws.push_back(tokens);
     }
-    int steps = 0;
-    int idx=0;
-    for (int j = 0; j < vec.size(); j++) {
-        if (vec[j][0] == "AAA") {
-            idx = j;
-            break;
+
+    std::vector<std::string> dirs = ws[0];
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> move;
+
+    for (size_t i = 2; i < ws.size(); ++i) {
+        const auto& start = ws[i][0];
+        const auto& l = ws[i][1];
+        const auto& r = ws[i][2];
+        move["L"][start] = l;
+        move["R"][start] = r;
+    }
+
+    auto length = [&](const std::string& here, bool part1) {
+        for (size_t i = 0;; ++i) {
+            const auto& d = dirs[i % dirs.size()];
+            const auto& next = move[d][here];
+
+            if ((part1 && next == "ZZZ") || (!part1 && next.back() == 'Z')) {
+                return i + 1;
+            }
+
+            here = next;
+        }
+    };
+
+    // Part 1
+    std::cout << "Part 1: " << length("AAA", true) << std::endl;
+
+    // Part 2
+    std::vector<std::string> starts;
+    for (size_t i = 2; i < ws.size(); ++i) {
+        const auto& start = ws[i][0];
+        if (start.back() == 'A') {
+            starts.push_back(start);
         }
     }
-    
 
-    string s;
-    while (vec[idx][0] != "ZZZ") {
-        for (int i = 0; i < in.size(); i++) {
-            steps++;
-            if (in[i] == 'L') {
-                s = vec[idx][1];
-            }
-            else {
-                s = vec[idx][2];
-            }
-            std::cout << "str:" << s << std::endl;
-            std::cout << "steps:" << steps << std::endl;
-            for (int j = 0; j < vec.size(); j++) {
-                if (vec[j][0] == s) {
-                    idx = j;
-                    break;
-                }
-            }
-        }
-    }
+    auto lcm = [](int a, int b) { return std::lcm(a, b); };
+    int result = std::accumulate(starts.begin(), starts.end(), 1, [&](int acc, const std::string& s) {
+        return lcm(acc, length(s, false));
+        });
 
-    std::cout << "Steps:" << steps << std::endl;
-
-
+    std::cout << "Part 2: " << result << std::endl;
 }
